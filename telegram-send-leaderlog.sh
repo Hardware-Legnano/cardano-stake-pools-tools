6a17e5a9d8fffa71a450718c7cf2be561852fedf
Dark345 <p.vasinton@hwlegnano.it>
2021-06-20 11:42:11 +0200

    added maxPerformance and epochSlotsIdeal

--- telegram-send-leaderlog.sh
+++ telegram-send-leaderlog.sh
@@ -3,14 +3,14 @@
 EPOCH=$(curl LEADERLOG.JSON | jq -r '.epoch')
 LEADER_AT=$(curl LEADERLOG.JSON | jq -r '.assignedSlots[].at')
 EXPECTED=$(curl LEADERLOG.JSON | jq -r '.epochSlotsIdeal')
-EXPECTED=$(echo "$EXPECTED*100" | bc)
+LUCK=$(curl LEADERLOG.JSON | jq -r '.maxPerformance')
 
 if test -z "$LEADER_AT" 
 then
 
 curl -X POST \
    -H 'Content-Type: application/json' \
-   -d '{"chat_id": "CHAT_ID", "text": "<b>LEADERLOG CALCULATION AND SCHEDULE</b>\n\n<b>EPOCH:</b> '$EPOCH'\n\n<b>Luck expected:</b> '$EXPECTED'%\n\n<b>Slots assigned:</b>\n\nNONE FOR THIS EPOCH &#128532;", "disable_notification": true, "parse_mode": "HTML"}' \
+   -d '{"chat_id": "CHAT_ID", "text": "<b>LEADERLOG CALCULATION AND SCHEDULE</b>\n\n<b>EPOCH:</b> '$EPOCH'\n\n<b>Blocks expected:</b> '$EXPECTED'\n<b>Luck:</b> '$LUCK' %\n\n<b>Slots assigned:</b>\n\nNONE FOR THIS EPOCH &#128532;", "disable_notification": true, "parse_mode": "HTML"}' \
    https://api.telegram.org/botAPIKEY/sendMessage
 
 else
@@ -20,7 +20,7 @@ LEADER_LINES=$(echo -e "$LEADER_LB" | wc -l)
 
 curl -X POST \
    -H 'Content-Type: application/json' \
-   -d '{"chat_id": "CHAT_ID", "text": "<b>LEADERLOG CALCULATION AND SCHEDULE</b>\n\n<b>EPOCH:</b> '$EPOCH'\n\n<b>Luck expected:</b> '$EXPECTED'%\n\n<b>Slots assigned: ('$LEADER_LINES') &#128512;</b>\n\n '$LEADER_LB'", "disable_notification": true, "parse_mode": "HTML"}' \
+   -d '{"chat_id": "CHAT_ID", "text": "<b>LEADERLOG CALCULATION AND SCHEDULE</b>\n\n<b>EPOCH:</b> '$EPOCH'\n\n<b>Blocks expected:</b> '$EXPECTED'\n<b>Luck:</b> '$LUCK' %\n\n<b>Slots assigned: ('$LEADER_LINES') &#128512;</b>\n\n '$LEADER_LB'", "disable_notification": true, "parse_mode": "HTML"}' \
    https://api.telegram.org/botAPIKEY/sendMessage
 
 
