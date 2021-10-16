882b7f8fb36c133f8c970b3838b856467dee70b1
Dark345 <p.vasinton@hwlegnano.it>
2021-10-16 15:19:30 +0200

    removed LEADER_LB from being transmitted
    publishing exact timing of leaderlog isn't a best practice

--- telegram-send-leaderlog.sh
+++ telegram-send-leaderlog.sh
@@ -20,7 +20,7 @@ LEADER_LINES=$(echo -e "$LEADER_LB" | wc -l)
 
 curl -X POST \
    -H 'Content-Type: application/json' \
-   -d '{"chat_id": "CHAT_ID", "text": "<b>LEADERLOG CALCULATION AND SCHEDULE</b>\n\n<b>EPOCH:</b> '$EPOCH'\n\n<b>Slots expected:</b> '$EXPECTED'\n<b>Maximum Luck:</b> '$LUCK' %\n\n<b>Slots assigned: ('$LEADER_LINES') &#128512;</b>\n\n '$LEADER_LB'", "disable_notification": true, "parse_mode": "HTML"}' \
+   -d '{"chat_id": "CHAT_ID", "text": "<b>LEADERLOG CALCULATION AND SCHEDULE</b>\n\n<b>EPOCH:</b> '$EPOCH'\n\n<b>Slots expected:</b> '$EXPECTED'\n<b>Maximum Luck:</b> '$LUCK' %\n\n<b>Slots assigned: ('$LEADER_LINES') &#128512;</b>\n", "disable_notification": true, "parse_mode": "HTML"}' \
    https://api.telegram.org/botAPIKEY/sendMessage
 
 
